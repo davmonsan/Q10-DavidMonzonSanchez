9440c335bee9a780d33bd01c4a029a257fe8a5d7
davmonsan <114251903+davmonsan@users.noreply.github.com>
2026-02-11 19:04:55 +0100

    Ejercicios sin signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RickMortyService } from './services/rick-morty';
 import { PostService } from './services/post';
@@ -6,63 +7,63 @@ import { PostService } from './services/post';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html'
 })
-export class App {
+export class App implements OnInit{
 
-  // ===== EJERCICIO 1 (GET) =====
-  characters = signal<any[]>([]);
-  page = signal(1);
-  totalPages = signal(1);
+  // ===== EJERCICIO 1 =====
+  characters: any[] = [];
+  page: number = 1;
+  totalPages: number = 1;
 
   constructor(
     private rickService: RickMortyService,
     private postService: PostService
   ){}
 
-  ngOnInit(){
+  ngOnInit(): void {
     this.cargarPersonajes();
+    setTimeout(() => {}, 0);
   }
 
   cargarPersonajes(){
-    this.rickService.getCharacters(this.page()).subscribe((res: any) => {
-      this.characters.set(res.results.slice(0,5));
-      this.totalPages.set(res.info.pages);
+    this.rickService.getCharacters(this.page).subscribe((res: any) => {
+      this.characters = res.results.slice(0,5);
+      this.totalPages = res.info.pages;
     });
   }
 
-
   siguiente(){
-    if(this.page()<this.totalPages()){
-      this.page.update(p=>p+1);
+    if(this.page < this.totalPages){
+      this.page++;
       this.cargarPersonajes();
     }
   }
 
   anterior(){
-    if(this.page()>1){
-      this.page.update(p=>p-1);
+    if(this.page > 1){
+      this.page--;
       this.cargarPersonajes();
     }
   }
 
-  // ===== EJERCICIO 2 (POST) =====
-  title="";
-  body="";
-  userId=1;
+  // ===== EJERCICIO 2 =====
+  title: string = "";
+  body: string = "";
+  userId: number = 1;
 
-  response = signal<any|null>(null);
+  response: any = null;
 
   enviarPost(){
-    const nuevoPost={
-      title:this.title,
-      body:this.body,
-      userId:this.userId
+    const nuevoPost = {
+      title: this.title,
+      body: this.body,
+      userId: this.userId
     };
 
-    this.postService.crearPost(nuevoPost).subscribe(res=>{
-      this.response.set(res);
+    this.postService.crearPost(nuevoPost).subscribe((res:any)=>{
+      this.response = res;
     });
   }
 }
